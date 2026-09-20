b1f60ffb6c1e2e4816c6758c73bee4f5406afb89
Amit Pandey <amitpandey1187170@gmail.com>
2026-09-20 14:01:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.java
+++ Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.java
@@ -2,23 +2,28 @@ import java.util.ArrayList;
 
 class Solution {
     ArrayList<Integer> find(int arr[], int x) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int left = 0, right = arr.length - 1;
-        int firstidx = -1;
-        int lastidx = -1; 
-
-        while (left <= right) { 
-            if (arr[left] == x && arr[right] == x) {
-                firstidx = left;
-                lastidx = right;
-                break;
-            }
-            if (arr[left] != x) left++;
-            if (arr[right] != x) right--;
-        }
-
-        list.add(firstidx);
-        list.add(lastidx);
-        return list;
+      ArrayList<Integer> list = new ArrayList<>();
+      
+      int left = 0, right = arr.length-1;
+      int firstidx=-1,secondidx=-1;
+      
+      while(left<=right){
+          if(arr[left] == x && arr[right]==x){
+              firstidx = left;
+              secondidx = right;
+              break;
+          }
+          
+          if(arr[left] != x){
+              left++;
+          }
+          
+          if(arr[right] != x){
+              right--;
+          }
+      }
+      list.add(firstidx);
+      list.add(secondidx);
+      return list;
     }
 }
