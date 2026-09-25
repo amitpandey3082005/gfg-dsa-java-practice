2f29a62a1e110bf5e2497d843bb3a83134dd3557
Amit Pandey <amitpandey1187170@gmail.com>
2026-09-26 00:30:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All Prime Factors in Sorted Order/all-prime-factors-in-sorted-order.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static void printPrimeFactorization(int n) {
+        // code here
+        
+        // checking for factor of two 
+        while(n%2==0){
+            System.out.print(2+" ");
+            n/=2;
+        }
+        
+        for(int i=3;i<=Math.sqrt(n);i+=2){
+            while(n%i==0){
+                System.out.print(i+" ");
+                n/=i;
+            }
+        }
+        
+        if(n>2){
+            System.out.print(n+" ");
+        }
+    }
+}
